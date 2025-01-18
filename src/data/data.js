a307600f68d25d26d3439035600ec89e19f91de7
fay-360 <aruoriwo.roraogor@ucalgary.ca>
2025-01-18 10:00:30 -0700

    I made changes yes whahoo :)

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,13 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "Aruoriwo Rora-Ogor",
+    name: "Aruoriwo Favour Rora-Ogor",
     bio: "Bachelor of Design in City Innovation Student at University of Calgary passionate about the intersection between Architecture and Psychology.",
     contact: {
         email: "aruoriwo.roraogor@ucalgary.ca",
         phone: "+3682990546",
         linkedin: "https://www.linkedin.com/",
-        github: "https://www.github.com/",
+        github: "https://www.github.com/fay-360",
     }
 }
 
